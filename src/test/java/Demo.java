fff04b64c47ce27282312232353210aa202d863c
Yoni Flenner <yoniflenner.net@gmail.com>
2020-04-07 10:35:49 +0300

    test

---
+++ src/test/java/Demo.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class Demo
+{
+    @Test
+    public void Test01()
+    {
+        System.out.println("Hello Jenkins Project, How are You Today ?");
+    }
+}
