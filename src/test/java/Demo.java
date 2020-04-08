3bd4e42e50c49172e6ccfc2d9cf6ee6597f261d6
yflenner <43785276+yflenner@users.noreply.github.com>
2020-04-08 08:16:16 +0300

    Demo.java
    Adding Selenium Test Case

--- src/test/java/Demo.java
+++ src/test/java/Demo.java
@@ -1,3 +1,6 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class Demo
@@ -6,5 +9,17 @@ public class Demo
     public void Test01()
     {
         System.out.println("Hello Jenkins Project, How are You Today ?");
+        System.out.println("Job Executed By Yoni");
+    }
+
+    @Test
+    public void Test02()
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        System.out.println("Title of WebPage is: " + driver.getTitle());
+        System.out.println("URL of WebPage is: " + driver.getCurrentUrl());
+        driver.quit();
     }
 }
